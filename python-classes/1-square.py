98b31718a55bb12f2844ef40b99f0a3f93b71ee6
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-09 11:08:08 +0000

    Class task 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a Square class with a private size attribute
-and validates the size during instantiation."""
+"""Define a Square class."""
+
+
 class Square:
-     """Class that defines a square with a private size attribute."""
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square's side.
+    """
+
     def __init__(self, size=0):
-                """Initialize the square with the given size.
+        """
+        Initializes a new Square instance.
 
         Args:
-            size (int): The size of the square (default 0).
+            size (int, optional): The size of the new square. Defaults to 0.
 
         Raises:
             TypeError: If size is not an integer.
