c7f90e0647062243e329320a58b830b590ced35f
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-09 11:02:37 +0000

    Class task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+                """Initialize the square with the given size.
+
+        Args:
+            size (int): The size of the square (default 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
