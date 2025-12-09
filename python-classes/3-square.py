86b3bf89ae487e8b0684af82758a115f82235006
rufet-zeynal <rufet.zeynalli2@gmail.com>
2025-12-09 11:41:50 +0000

    Class task 4

---
+++ python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Creating Square class
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+        """
+        self.size = size
+    @property
+    def size(self):
+        """
+        Retrieve the size
+        """
+        return self.__size
+    @sizesetter
+    def size(self, value):
+        """
+        Setter to set the size
+        Args:
+            value (int): The new size for the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """
+        Area of the square
+        """
+        return self.__size **2
